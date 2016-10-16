410794b16ee9a941c3a2f1fa1271ffd2754c80c0
David Paul Silverstone <dsilvers@usc.edu>
2016-10-15 23:31:36 -0700

    Person class

---
+++ src/com/dSilversKevinWuIshaSharmaLaurissaLam/Patient.java
@@ -0,0 +1,11 @@
+package com.dSilversKevinWuIshaSharmaLaurissaLam;
+
+public class Patient extends Person {
+	
+	protected Person[] mNextOfKin;
+	protected HealthCareProvider primaryCarePhysician;
+	
+	
+	
+	
+}
--- src/com/dSilversKevinWuIshaSharmaLaurissaLam/Person.java
+++ src/com/dSilversKevinWuIshaSharmaLaurissaLam/Person.java
@@ -1,7 +1,29 @@
 package com.dSilversKevinWuIshaSharmaLaurissaLam;
 
+import java.util.Date;
+
 public class Person {
 
+	protected String _name;
+	protected Date _dob;
+	protected int _ID;
 	
+	public String getName() {
+		return _name;
+	}
 	
-}
+	public Date getDate( ) {
+		return _dob;
+	}
+	
+	public int getID() {
+		return _ID;
+	}
+	
+	Person(String name, Date birthdate, int socSec) {
+		_name = name;
+		_dob = birthdate;
+		_ID = socSec;
+	}
+	
+};
