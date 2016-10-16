9e5806d28eac83a847547e0c86aa7a08393c092b
David Paul Silverstone <dsilvers@usc.edu>
2016-10-16 10:19:15 -0700

    le

--- Nurse.hpp
+++ Nurse.hpp
@@ -11,21 +11,24 @@
 
 #include "HealthCareProvider"
 
+class Doctor; // forward declaration
+
 class Nurse : public HealthCareProvider {
 private:
 public:
-    Nurse(std::string Name, int ID, Institution* inst = 0)
+    Nurse(std::string Name, int ID, Institution* inst = 0, Doctor* dr = 0)
     : HealthCareProvider(Name, ID, inst)
     {
-
+        supervisor = dr;
     };
     ~Nurse();
     bool hasAccessTo(Patient* pPatient) {
         // a little more lenient on which patients nurses can see records of than Physician's Assistant
+        // nurse can access records of patients at their division or
         return false;
     }
     bool canAccessTheseRecords(recordClass rc) {
-        return (rc == PHYSICAL || rc == ALLERGY || rc == SURGERY || rc == PRESCRIPTION || rc == HOSPITALIZATION);
+        return (rc == recordClass::PHYSICAL || rc == recordClass::ALLERGY || rc == recordClass::SURGERY || rc == recordClass::PRESCRIPTION || rc == recordClass::HOSPITALIZATION);
     }
     bool hasAccessTo(Patient* pPatient, recordClass rc) {
         return (hasAccessTo(pPatient) && canAccessTheseRecords(rc));
@@ -34,7 +37,7 @@ public:
         return "Nurse";
     }
 protected:
-
+    Doctor* supervisor;
 };
 
 
